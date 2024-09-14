cc9158627b134d8f6d176dd3ff3ee8158ed6bc90
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2024-09-14 08:43:15 +0530

    test4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>AI Version Test</h1>'
+    return '<h1>AI Version Test1</h1>'
 
 if __name__ == '__main__':
     #app.run(debug=True)
